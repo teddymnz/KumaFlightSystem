c02a467b7d667aebb6946e90b39f51b67aba7b65
teddymnz <teddy.marieinez@gmail.com>
2026-09-21 18:39:55 +0900

    refactor: migrate application layer to C++

--- CM7/Core/Inc/FreeRTOSConfig.h
+++ CM7/Core/Inc/FreeRTOSConfig.h
@@ -67,7 +67,7 @@ extern uint32_t SystemCoreClock;
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 56 )
 #define configUSE_SB_COMPLETED_CALLBACK          ( 0 )
-#define configUSE_MINI_LIST_ITEM                ( 1 )
+#define configUSE_MINI_LIST_ITEM                 ( 1 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
 #define configTOTAL_HEAP_SIZE                    ((size_t)15360)
 #define configMAX_TASK_NAME_LEN                  ( 16 )

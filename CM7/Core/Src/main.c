e4e1519183a3e9224d186a9d1f2593b324a43a3f
teddymnz <teddy.marieinez@gmail.com>
2026-09-25 18:50:24 +0900

    feat: bring up FreeRTOS on CM4

--- CM7/Core/Src/main.c
+++ CM7/Core/Src/main.c
@@ -103,9 +103,9 @@ int main(void)
   /* MPU Configuration--------------------------------------------------------*/
   MPU_Config();
 
+/* USER CODE BEGIN Boot_Mode_Sequence_1 */
 // TODO:
 // Temporarily disable CM7/CM4 boot synchronization while developing the CM7 firmware only.
-/* USER CODE BEGIN Boot_Mode_Sequence_1 */
 //#if defined(DUAL_CORE_BOOT_SYNC_SEQUENCE)
 //  /* Wait until CPU2 boots and enters in stop mode or timeout*/
 //  timeout = 0xFFFF;
